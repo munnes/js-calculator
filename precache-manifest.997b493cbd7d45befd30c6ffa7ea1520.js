96be5bbcd0993f2eee42757a935b650273554bdf
Nasreen Sayed Siddig Elhussain <nasreen.store@gmail.com>
2019-10-22 14:35:32 +0400

    Updates

---
+++ precache-manifest.997b493cbd7d45befd30c6ffa7ea1520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3b16af3e50e5f1d926a2cecfbc5e39",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "b69aa515a8a59e0ceaa6",
+    "url": "/js-calculator/static/css/main.635d7f82.chunk.css"
+  },
+  {
+    "revision": "9ea28c3f46d869129905",
+    "url": "/js-calculator/static/js/2.cbcfb808.chunk.js"
+  },
+  {
+    "revision": "b69aa515a8a59e0ceaa6",
+    "url": "/js-calculator/static/js/main.718b80e1.chunk.js"
+  },
+  {
+    "revision": "26b2f31f0bda2d33c55d",
+    "url": "/js-calculator/static/js/runtime-main.98890ba2.js"
+  }
+]);
